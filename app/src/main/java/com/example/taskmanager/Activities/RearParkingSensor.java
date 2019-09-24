327cc084015ce62e30dad56411a7d635f6b3fb24
Andrej Popordanoski <andrejpopordanoski@MacBookPros-MacBook-Pro.local>
2019-09-24 23:12:59 +0200

    rear parking sensor

---
+++ app/src/main/java/com/example/taskmanager/Activities/RearParkingSensor.java
@@ -0,0 +1,16 @@
+package com.example.taskmanager.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.taskmanager.R;
+
+public class RearParkingSensor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rear_parking_sensor);
+    }
+}
