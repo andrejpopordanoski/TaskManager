7a90c8fc7d28bfe3f68ff428108e6f2328682df9
Christian Stopbones <h.stojkoski32@gmail.com>
2019-09-25 00:15:03 +0200

    added symbol

--- app/src/main/java/com/example/taskmanager/Activities/RearParkingSensor.java
+++ app/src/main/java/com/example/taskmanager/Activities/RearParkingSensor.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.taskmanager.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RearParkingSensor extends AppCompatActivity {
 
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
