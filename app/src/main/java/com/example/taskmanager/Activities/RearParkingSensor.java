e9f328e4439b23963a6e0ffe87e2a1a72e9573e2
Andrej Popordanoski <andrejpopordanoski@MacBookPros-MacBook-Pro.local>
2019-09-26 01:15:40 +0200

    finishing project

--- app/src/main/java/com/example/taskmanager/Activities/RearParkingSensor.java
+++ app/src/main/java/com/example/taskmanager/Activities/RearParkingSensor.java
@@ -8,6 +8,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -24,11 +25,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class RearParkingSensor extends AppCompatActivity {
-    TextView distanceMeter;
+    TextView distanceMeterLeft, distanceMeterRight;
     LinearLayout wall;
-    DatabaseReference distanceLeft;
-    float distanceRight;
-    float differenceBetweenSensors;
+    DatabaseReference distanceLeftRef, distanceRightRef;
+
+    float differenceBetweenSensors, distanceLeft, distanceRight, closestDistance;
+    boolean leftIsCloser;
     ImageView leftIndicator,rightIndicator;
     LinearLayout car;
 
@@ -37,37 +39,39 @@ public class RearParkingSensor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rear_parking_sensor);
-        distanceLeft = FirebaseDatabase.getInstance().getReference().child("distance");
-        distanceMeter = findViewById(R.id.distance_meter);
+        distanceLeftRef = FirebaseDatabase.getInstance().getReference().child("distanceRight");
+        distanceRightRef = FirebaseDatabase.getInstance().getReference().child("distanceLeft");
+
+        distanceMeterLeft = findViewById(R.id.distance_meter_left);
+        distanceMeterRight = findViewById(R.id.distance_meter_right);
+
         wall = findViewById(R.id.wall);
         leftIndicator = findViewById(R.id.left_indicator);
         rightIndicator = findViewById(R.id.right_indicator);
         car = findViewById(R.id.car);
-        distanceRight = 50f;
-        differenceBetweenSensors = 30;
 
-        distanceLeft.addValueEventListener(new ValueEventListener() {
+        differenceBetweenSensors = 22;
+        distanceLeftRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                float distanceLeft = dataSnapshot.getValue(Float.class);
-                distanceMeter.setText(distanceLeft + "cm");
-                if (distanceLeft<=40){
-                    activateCloseDistance(distanceLeft);
-                    car.setTranslationY((360 - distanceLeft*9));
-
-                }else {
-                    vehicleSafeZone();
-                    car.setTranslationY(0);
+                distanceLeft = dataSnapshot.getValue(Float.class);
+                distanceMeterLeft.setText(distanceLeft + "cm");
+                closestDistance = (distanceLeft>distanceRight)?distanceRight:distanceLeft;
+                leftIsCloser = (distanceLeft<distanceRight)?true:false;
+                if (closestDistance<=60){
+                    activateCloseDistance(closestDistance, leftIsCloser);
+                    float dps = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 130 - closestDistance*3, getResources().getDisplayMetrics());
+                    car.setTranslationY(dps);
 
                 }
-                float difference = Math.abs(distanceLeft - distanceRight);
-                double hipotenuse = Math.sqrt( Math.pow(difference, 2) + Math.pow(differenceBetweenSensors, 2));
-                double degree = Math.toDegrees(Math.sin(difference/hipotenuse));
-                Log.i("SINUSE", degree + "");
-                if(distanceLeft > distanceRight)
-                    car.setRotation((float) degree);
-                else
-                    car.setRotation((float) -degree);
+                if(closestDistance>60){
+                    vehicleSafeZone(true);
+                }
+                if(distanceLeft >60){
+                    resetBySide(true);
+                }
+
+
             }
 
             @Override
@@ -76,25 +80,95 @@ public class RearParkingSensor extends AppCompatActivity {
             }
         });
 
+
+        distanceRightRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                distanceRight = dataSnapshot.getValue(Float.class);
+                distanceMeterRight.setText(distanceRight + "cm");
+                closestDistance = (distanceLeft>distanceRight)?distanceRight:distanceLeft;
+                leftIsCloser = (distanceLeft<distanceRight)?true:false;
+
+                if (closestDistance<=60){
+                    activateCloseDistance(closestDistance, leftIsCloser);
+                    float dps = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 130 - closestDistance*3, getResources().getDisplayMetrics());
+                    car.setTranslationY(dps);
+
+                }
+                if(closestDistance>60){
+                    vehicleSafeZone(false);
+                }
+                if(distanceRight >60){
+                    resetBySide(false);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
-    public void activateCloseDistance(double distanceLeft) {
+    public void activateCloseDistance(double distanceLeft, boolean leftIsCloser) {
+
+
+        float difference = Math.abs(this.distanceLeft - this.distanceRight);
+        double hipotenuse = Math.sqrt( Math.pow(difference, 2) + Math.pow(differenceBetweenSensors, 2));
+        double degree = Math.toDegrees(Math.sin(difference/hipotenuse));
+
+        if(this.distanceLeft > this.distanceRight)
+            car.setRotation((float) degree);
+        else
+            car.setRotation((float) -degree);
+
         Animation animation = new AlphaAnimation(1, 0); //to change visibility from visible to invisible
         animation.setDuration((long)distanceLeft*20); //1 second duration for each animation cycle
         animation.setInterpolator(new LinearInterpolator());
         animation.setRepeatCount(Animation.INFINITE); //repeating indefinitely
         animation.setRepeatMode(Animation.REVERSE); //animation will start from end point once ended.
-        leftIndicator.startAnimation(animation); //to start animation
-        leftIndicator.setColorFilter(Color.argb(255, 173, 0, 63));
-        distanceMeter.setTextColor(Color.argb(255, 173, 0, 63));
+        if(leftIsCloser) {
+            leftIndicator.startAnimation(animation); //to start animation
+            leftIndicator.setColorFilter(Color.argb(255, 173, 0, 63));
+            distanceMeterLeft.setTextColor(Color.argb(255, 173, 0, 63));
+        }
+        else {
+            rightIndicator.startAnimation(animation); //to start animation
+            rightIndicator.setColorFilter(Color.argb(255, 173, 0, 63));
+            distanceMeterRight.setTextColor(Color.argb(255, 173, 0, 63));
+        }
+
+
 //                    distanceMeter.startAnimation(animation);
     }
 
-    public void vehicleSafeZone() {
-        leftIndicator.clearAnimation();
-//                    distanceMeter.clearAnimation();
+    public void vehicleSafeZone(boolean left) {
 
-        leftIndicator.setColorFilter(Color.argb(255,163,171,184));
-        distanceMeter.setTextColor(Color.argb(255, 255, 255, 255));
+            car.setRotation(0);
+            car.setTranslationY(0);
+            leftIndicator.clearAnimation();
+            leftIndicator.setColorFilter(Color.argb(255,163,171,184));
+            distanceMeterLeft.setTextColor(Color.argb(255, 255, 255, 255));
+
+
+            rightIndicator.clearAnimation();
+            rightIndicator.setColorFilter(Color.argb(255, 163, 171, 184));
+            distanceMeterRight.setTextColor(Color.argb(255, 255, 255, 255));
+
+
+    }
+
+    public void resetBySide(boolean left) {
+        if(left) {
+            leftIndicator.clearAnimation();
+            leftIndicator.setColorFilter(Color.argb(255, 163, 171, 184));
+            distanceMeterLeft.setTextColor(Color.argb(255, 255, 255, 255));
+        }
+        else {
+            rightIndicator.clearAnimation();
+            rightIndicator.setColorFilter(Color.argb(255, 163, 171, 184));
+            distanceMeterRight.setTextColor(Color.argb(255, 255, 255, 255));
+        }
     }
 }
